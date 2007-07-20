3141e94bb6100e2de2a06a8e2e8b61e6f556d6a3
schani <schani>
2007-07-20 13:28:23 +0000

    Added consume function.

--- dna2rna/dnabuf.ml
+++ dna2rna/dnabuf.ml
@@ -3,30 +3,30 @@ type 'a hbuf =
   | SubBuf of 'a hbuf * int * int
   | ConcatBuf of 'a hbuf * 'a hbuf * int;;
 
-(* val create : 'a array -> 'a hbuf;; *)
+(* val create : 'a array -> 'a hbuf *)
 let create arr =
   ArrayBuf arr;;
 
-(* val length : 'a hbuf -> int;; *)
+(* val length : 'a hbuf -> int *)
 let rec length = function
     ArrayBuf arr -> Array.length arr
   | SubBuf (_, start, stop) -> stop - start
   | ConcatBuf (_, _, len) -> len;;
 
-(* val subbuf : 'a hbuf -> int -> int -> 'a hbuf;; *)
+(* val subbuf : 'a hbuf -> int -> int -> 'a hbuf *)
 let subbuf hbuf start stop =
   (assert (stop >= start);
    assert (start >= 0);
    assert (stop <= (length hbuf));
    SubBuf (hbuf, start, stop));;
 
-(* val concat : 'a hbuf -> 'a hbuf -> 'a hbuf;; *)
+(* val concat : 'a hbuf -> 'a hbuf -> 'a hbuf *)
 let concat buf1 buf2 =
   let len1 = length buf1
   and len2 = length buf2
   in ConcatBuf (buf1, buf2, (len1 + len2));;
 
-(* val nth : 'a hbuf -> int -> 'a;; *)
+(* val nth : 'a hbuf -> int -> 'a *)
 let rec nth hbuf i =
   (assert (i >= 0);
    assert (i < (length hbuf));
@@ -39,3 +39,21 @@ let rec nth hbuf i =
 	     nth buf1 i
 	   else
 	     nth buf2 (i - len1));;
+
+(* val consume : 'a hbuf -> ('a * ('a hbuf)) option *)
+let consume hbuf =
+  let rec rest hbuf =
+    let len = length hbuf
+    in match hbuf with
+	ArrayBuf arr -> SubBuf (hbuf, 1, len)
+      | SubBuf (sub, start, stop) -> SubBuf (sub, start + 1, stop)
+      | ConcatBuf (buf1, buf2, len) ->
+	  if (length buf1) < 1 then
+	    rest buf2
+	  else
+	    ConcatBuf (rest buf1, buf2, len - 1)
+  in    
+    if (length hbuf) > 0 then
+      Some (nth hbuf 0, rest hbuf)
+    else
+      None;;
