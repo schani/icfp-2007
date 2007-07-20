ee0e8397ebce06528f94f4747597217c7d880e0c
schani <schani>
2007-07-20 14:30:53 +0000

    Efficient implementation of concat_base.

--- dna2rna/dna.ml
+++ dna2rna/dna.ml
@@ -2,6 +2,4 @@ open Dnabuf;;
 
 (* val read_dna : in_channel -> dna *)
 let read_dna file =
-    let buf = Buffer.create 1048576
-    in (Buffer.add_channel buf file;
-	create (Buffer.contents buf))
+  create (input_line file);;
--- dna2rna/dnabuf.ml
+++ dna2rna/dnabuf.ml
@@ -3,17 +3,39 @@ exception Invalid_base_char;;
 type base = I | C | F | P;;
 
 type dna =
-    ArrayBuf of string
+    ArrayBuf of Buffer.t * int
   | SubBuf of dna * int * int
   | ConcatBuf of dna * dna * int;;
 
+let char_to_base = function
+    'I' -> I
+  | 'C' -> C
+  | 'F' -> F
+  | 'P' -> P
+  | _ -> raise Invalid_base_char;;
+
+let base_to_char = function
+    I -> 'I'
+  | C -> 'C'
+  | F -> 'F'
+  | P -> 'P';;
+
 (* val create : string -> dna *)
-let create arr =
-  ArrayBuf arr;;
+let create str =
+  let len = String.length str
+  in let buf = Buffer.create len
+  in (Buffer.add_string buf str;
+      ArrayBuf (buf, len));;
+
+(* val dna_from_base : base -> dna *)
+let dna_from_base b =
+  let buf = Buffer.create 16
+  in (Buffer.add_char buf (base_to_char b);
+      ArrayBuf (buf, 1));;
 
 (* val length : dna -> int *)
 let rec length = function
-    ArrayBuf arr -> String.length arr
+    ArrayBuf (_, len) -> len
   | SubBuf (_, start, stop) -> stop - start
   | ConcatBuf (_, _, len) -> len;;
 
@@ -30,19 +52,26 @@ let concat buf1 buf2 =
   and len2 = length buf2
   in ConcatBuf (buf1, buf2, (len1 + len2));;
 
-let char_to_base = function
-    'I' -> I
-  | 'C' -> C
-  | 'F' -> F
-  | 'P' -> P
-  | _ -> raise Invalid_base_char;;
+(* val concat_base : dna -> base -> dna *)
+let rec concat_base buf b =
+  match buf with
+      ArrayBuf (arr, len) ->
+	if (Buffer.length arr) = len then
+	  (Buffer.add_char arr (base_to_char b);
+	   ArrayBuf (arr, len + 1))
+	else
+	  ConcatBuf (buf, dna_from_base b, len + 1)
+    | SubBuf (sub, start, stop) ->
+	ConcatBuf (buf, dna_from_base b, stop - start + 1)
+    | ConcatBuf (buf1, buf2, len) ->
+	ConcatBuf (buf1, concat_base buf2 b, len + 1);;
 
 (* val nth : dna -> int -> base *)
 let rec nth hbuf i =
   (assert (i >= 0);
    assert (i < (length hbuf));
    match hbuf with
-       ArrayBuf arr -> char_to_base (String.get arr i)
+       ArrayBuf (arr, len) -> char_to_base (Buffer.nth arr i)
      | SubBuf (hbuf, start, _) -> nth hbuf (i - start)
      | ConcatBuf (buf1, buf2, _) ->
 	 let len1 = length buf1
@@ -58,7 +87,7 @@ let rec skip hbuf i =
       SubBuf (hbuf, len, len)
     else
       match hbuf with
-	  ArrayBuf arr -> SubBuf (hbuf, i, len)
+	  ArrayBuf (arr, len) -> SubBuf (hbuf, i, len)
 	| SubBuf (sub, start, stop) -> SubBuf (sub, start + i, stop)
 	| ConcatBuf (buf1, buf2, len) ->
 	    let len1 = length buf1
